aa24ffb0905bf45eba080f63d8af128a03aa4dec
Meng Zhuo <mengzhuo1203@gmail.com>
2024-08-05 19:55:36 +0800

    fix python-dev

--- ubuntu/scripts/install-noble.sh
+++ ubuntu/scripts/install-noble.sh
@@ -7,7 +7,7 @@ apt-get install build-essential -yq --no-install-recommends
 apt-get install cmake ninja-build -yq --no-install-recommends
 
 apt-get install llvm llvm-dev llvm-runtime -yq --no-install-recommends
-apt-get install python3 python3-dev pipx -yq --no-install-recommends
+apt-get install python3 python3-dev python3-virtualenv pipx -yq --no-install-recommends
 apt-get install nodejs npm -yq --no-install-recommends
 apt-get install golang -yq --no-install-recommends
 apt-get install rustc cargo -yq --no-install-recommends
